ea9ab1d742f50bad3c54c74604949f4693df9918
Kevin Bagnall <bubbagump2020@gmail.com>
2019-08-07 13:55:53 +0000

    Automatically backed up by Learn

--- meal_choice.rb
+++ meal_choice.rb
@@ -1,9 +1,5 @@
 # Define a method meal_choice that returns the meal_choice that was
 # passed into it and defaults to meat.
 def meal_choice (meat_choice)
-  if meat_choice == nil
-    meat_choice = "meat"
-  else
-    return meat_choice
-  end
+  meat_choice ||:
 end
